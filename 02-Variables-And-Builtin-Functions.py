e913ee316ae22a98ee219dd1eafde49dbd8c7bd7
Ammaee Malik <awanmmar122@gmail.com>
2026-07-28 01:56:40 +0500

    StepUp-Day2 Branch

---
+++ 02-Variables-And-Builtin-Functions.py
@@ -0,0 +1,14 @@
+x = 5 
+y = x + 3
+z = x * y + 8
+
+numStr = "10.6"
+complexNum = 1 + 2j
+
+print(float(numStr))
+print(int(float(numStr)))
+print(list(numStr))
+print(complexNum * complexNum)
+print(complex(z + 1 + 10j))
+
+print(z)
