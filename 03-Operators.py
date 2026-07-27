026118103535db327deec17598c971b17bea582d
Ammar Malik <awanmmar122@gmail.com>
2026-07-28 03:51:53 +0500

    Completed Day-03

--- 03-Operators.py
+++ 03-Operators.py
@@ -2,7 +2,34 @@
 print(True)
 print(False)
 
-a = 100
-b = 256
+print('True == True: ', True == True)
+print('True == False: ', True == False)
+print('False == False:', False == False)
+
+# Excercise
+age = 20
+height = 1.93
+complexity = 1 + 2j
+
+a = 10
+b = 5  
+print("a = 10 , b = 5")
+print("a == b" , a == b)
+print("a != b" , a != b)
+print("a > b" , a > b)
+print("a < b" , a < b)
+print("a >= b" , a >= b)
+print("a <= b" , a <= b)
+print("a is b" , a is b)
+print("a is not b" , a is not b)
+
+num = int(input('Enter a number: '))
+print(num, num**0, num**1, num**2, num**3)
 
 
+base = float(input('Enter the Length of The Base: '))
+rectHeight = float(input('Enter the Height of The Rectangle: '))
+area = base * rectHeight
+perimeter = 2 * (base + rectHeight)
+print('The area of the rectangle is: ', int(area))
+print('The perimeter of the rectangle is: ', int(perimeter))
