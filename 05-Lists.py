9e4ab464879a5d4f61255227794b965fa46fbbc2
Ammar Malik <work.malikammar@gmail.com>
2026-08-02 14:13:13 +0500

    Just Trying The New Email

--- 05-Lists.py
+++ 05-Lists.py
@@ -1,1 +1,6 @@
-List = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+Fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+print("Fruits List:", Fruits)
+print("First Fruit:", Fruits[0])
+print("Second Last Fruit:", Fruits[-2])
+print("Last Fruit:", Fruits[len(Fruits)-1])
+print("Fruits from index 1 to 3:", Fruits[1:4])
